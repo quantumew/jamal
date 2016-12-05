f2d05597fb03428b39919ccffbff3e105f74aab7
Andy Gertjejansen <AndyGertjejansen@outlook.com>
2016-12-05 17:34:29 -0600

    Add simple README and fix some issues with the CLI.

---
+++ README.md
@@ -0,0 +1,29 @@
+Jamal
+=====
+
+YAML to JSON and JSON to YAML conversion tool. Written in Golang so it can be statically compiled and dropped anywhere you need it.
+
+[Build of latest release.](https://github.com/quantumew/jamal/releases)
+
+
+Usage:
+        jamal <action> [<input-file>]
+
+    Options:
+        -h --help       Show this message.
+
+    Arguments:
+        <action>        Conversion action. [yaml2json|json2yaml]
+
+        <input-file>    Path to data file.
+
+Examples:
+
+    # Output YAML from a JSON file.
+    jamal json2yaml some-file.json
+
+    # Output JSON from a YAML file.
+    jamal yaml2json some-file.yaml
+
+    # Output YAML from JSON from STDIN.
+    echo '{"property": 5, "otherThing": true}' | jamal json2yaml
--- jamal.go
+++ jamal.go
@@ -2,10 +2,10 @@
 package main
 
 import (
-    "errors"
+	"encoding/json"
+	"errors"
 	"github.com/docopt/docopt-go"
 	"github.com/ghodss/yaml"
-    "encoding/json"
 	"io/ioutil"
 	"log"
 	"os"
@@ -14,58 +14,67 @@ import (
 var logger = log.New(os.Stderr, "", 0)
 
 func main() {
-    doc := `Jamal
+	doc := `Jamal
 
         Command line interface for converting JSON to YAML and YAML to JSON.
         Expects either a input file or data from stdin.
 
         Usage:
-            jamal [<action>] [<input-file>]
+            jamal <action> [<input-file>]
 
         Options:
             -h --help       Show this message.
 
         Arguments:
-        <action>        Conversion action. [yaml2json | json2yaml] [default: json2yaml]
+            <action>        Conversion action. [yaml2json|json2yaml]
 
             <input-file>    Path to data file.
     `
-    arguments, _ := docopt.Parse(doc, nil, true, "Jamal 1.0.0", false)
-    dataPath := arguments["<input-file>"]
-    action := arguments["<action>"]
+	arguments, _ := docopt.Parse(doc, nil, true, "Jamal 1.0.0", false)
+	dataPath := arguments["<input-file>"]
+	action := arguments["<action>"]
 
-    var (
-        err error
-        data []byte
-        decodedData []byte
-    )
+	var (
+		err         error
+		data        []byte
+		decodedData []byte
+	)
 
-    if dataPath == nil {
-        data, err = readStdin()
-    } else {
-        path := dataPath.(string)
-        data, err = ioutil.ReadFile(path)
-    }
+	if (action == nil) || (action.(string) != "yaml2json" && action.(string) != "json2yaml") {
+		logger.Println("Invalid action.")
+		logger.Println(doc)
 
-    if err != nil {
-        logError("Error occurred loading data.", err)
+		os.Exit(1)
+	} else {
+		action = action.(string)
+	}
 
-        os.Exit(1)
-    }
+	if dataPath == nil {
+		data, err = readStdin()
+	} else {
+		path := dataPath.(string)
+		data, err = ioutil.ReadFile(path)
+	}
 
-    if action == "yaml2json" {
-        decodedData, err = yamlToJson(data)
-    } else {
-        decodedData, err = jsonToYaml(data)
-    }
+	if err != nil {
+		logError("Error occurred loading data.", err)
 
-    if err != nil {
-        logError("Error occurred converting data.", err)
+		os.Exit(1)
+	}
 
-        os.Exit(1)
-    }
+	if action == "yaml2json" {
+		decodedData, err = yamlToJson(data)
+	} else if action == "json2yaml" {
+		decodedData, err = jsonToYaml(data)
+	}
 
-    os.Stdout.Write(decodedData)
+	if err != nil {
+		logError("Error occurred converting data.", err)
+
+		os.Exit(1)
+	}
+
+	os.Stdout.Write(decodedData)
 }
 
 // Converts YAML to JSON.
@@ -74,13 +83,13 @@ func yamlToJson(raw []byte) ([]byte, error) {
 
 	err := yaml.Unmarshal(raw, &data)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    output, err := json.Marshal(data)
+	output, err := json.Marshal(data)
 
-    return output, err
+	return output, err
 }
 
 // Converts JSON to YAML.
@@ -89,32 +98,32 @@ func jsonToYaml(raw []byte) ([]byte, error) {
 
 	err := json.Unmarshal(raw, &data)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    output, err := yaml.Marshal(data)
+	output, err := yaml.Marshal(data)
 
-    return output, err
+	return output, err
 }
 
 func readStdin() ([]byte, error) {
 	fi, err := os.Stdin.Stat()
 
 	if err != nil {
-        return nil, err
+		return nil, err
 	}
 
-    if fi.Mode() & os.ModeNamedPipe == 0 {
-        err = errors.New("Nothing piped into stdin.")
+	if fi.Mode()&os.ModeNamedPipe == 0 {
+		err = errors.New("Nothing piped into stdin.")
 
-        return nil, err
-    }
+		return nil, err
+	}
 
-    return ioutil.ReadAll(os.Stdin)
+	return ioutil.ReadAll(os.Stdin)
 }
 
 func logError(msg string, err error) {
-    logger.Println(msg)
-    logger.Println(err.Error())
+	logger.Println(msg)
+	logger.Println(err.Error())
 }
